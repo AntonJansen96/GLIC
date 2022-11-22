6990da59254f7450ef1a6438ed5df33225510eb1
Anton Jansen <antonjansen96@gmail.com>
2022-11-22 20:15:45 +0100

    performed some testing

--- analysis/bloom/bloomData.py
+++ analysis/bloom/bloomData.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import MDAnalysis as mda
 
@@ -8,7 +9,7 @@ def simple_dist(coord1, coord2):
 
 dataset = '4HFI_4_1'  # not sure whether this variable is necessary
 top     = '../../sims/4HFI_4/01/CA.pdb'
-trajs   = ['../../sims/4HFI_4/01/MD.xtc']
+trajs   = ['../../sims/4HFI_4/01/MD_conv.xtc']
 SAVE    = True
 
 ecd_twist            = []
@@ -62,8 +63,11 @@ for traj in trajs:
         chains[i].append(ca)
         previous = current
 
-    # Make each chain an AtomGroup
+    # Make each chain an AtomGroup. chains is not now a list of 5 AtomGroup
+    # objects (one for each subunit), each containing 311 (CA) atoms.
     chains = [mda.AtomGroup(chains[i]) for i in range(0, len(chains))]
+
+    #? SO FAR SO GOOD
 
     for j in range(0, nframes):
 
@@ -173,34 +177,63 @@ for traj in trajs:
         minus_two_prime_pore.append(minus_two_prime_pore1[k])
         c_loop.append(c_loop1[k])
 
-if SAVE:
-    ecd_twist = np.array(ecd_twist)
-    ecd_spread = np.array(ecd_spread)
-    ecd_upper_spread = np.array(ecd_upper_spread)
-    beta_expansion = np.array(beta_expansion)
-    m2_m1_dist = np.array(m2_m1_dist)
-    m2_radius = np.array(m2_radius)
-    m1_kink = np.array(m1_kink)
-    m1_kink_alt = np.array(m1_kink_alt)
-    nine_prime_dist = np.array(nine_prime_dist)
-    nine_prime_pore = np.array(nine_prime_pore)
-    minus_two_prime_dist = np.array(minus_two_prime_dist)
-    minus_two_prime_pore = np.array(minus_two_prime_pore)
-    c_loop = np.array(c_loop)
+    # print(len(m2_radius))
+    # print(len(m2_radius[0]))
+    # print(len(m2_radius[1]))
+    # print(m2_radius[0][1:])
+    # print(m2_radius[0][0])
+    # print(len(m2_radius[0][1:]))
 
-    '''
-    np.save("ecd_twist_" + dataset + ".npy", ecd_twist)
-    np.save("ecd_spread_" + dataset + ".npy", ecd_spread)
-    np.save("ecd_upper_spread_" + dataset + ".npy", ecd_upper_spread)
-    np.save("beta_expansion_" + dataset + ".npy", beta_expansion)
-    np.save("m2_m1_dist_" + dataset + ".npy", m2_m1_dist)
-    np.save("m2_radius_" + dataset + ".npy", m2_radius)
-    np.save("m1_kink_" + dataset + ".npy", m1_kink)
-    np.save("m1_kink_alt_" + dataset + ".npy", m1_kink_alt)
-    np.save("nine_prime_" + dataset + ".npy", nine_prime_dist)
-    np.save("nine_prime_pore_" + dataset + ".npy", nine_prime_pore)
-    np.save("minus_two_prime_" + dataset + ".npy", minus_two_prime_dist)
-    np.save("minus_two_prime_pore_" + dataset + ".npy", minus_two_prime_pore)
-    np.save("c_loop_" + dataset + ".npy", c_loop)
-    '''
-    np.save("m2_spread_" + dataset + ".npy", m2_radius)
+    chains = ['A', 'B', 'C', 'D', 'E']
+    for idx in range(0, len(chains)):
+
+        t = range(0, len(m2_radius[idx][1:]))
+        x = m2_radius[idx][1:]
+        plt.plot(t, x, label=chains[idx])
+
+    plt.legend()
+    plt.xlabel('Time (ns)')
+    plt.ylabel('Distance (A??)')
+    plt.title('4HFI_4 1 chain A m2_radius')
+    plt.tight_layout()
+    plt.savefig('m2_radius_test.png')
+
+# if SAVE:
+#     ecd_twist = np.array(ecd_twist)
+#     ecd_spread = np.array(ecd_spread)
+#     ecd_upper_spread = np.array(ecd_upper_spread)
+#     beta_expansion = np.array(beta_expansion)
+#     m2_m1_dist = np.array(m2_m1_dist)
+#     m2_radius = np.array(m2_radius)
+#     m1_kink = np.array(m1_kink)
+#     m1_kink_alt = np.array(m1_kink_alt)
+#     nine_prime_dist = np.array(nine_prime_dist)
+#     nine_prime_pore = np.array(nine_prime_pore)
+#     minus_two_prime_dist = np.array(minus_two_prime_dist)
+#     minus_two_prime_pore = np.array(minus_two_prime_pore)
+#     c_loop = np.array(c_loop)
+
+#     print(m2_radius)
+
+#     '''
+#     np.save("ecd_twist_" + dataset + ".npy", ecd_twist)
+#     np.save("ecd_spread_" + dataset + ".npy", ecd_spread)
+#     np.save("ecd_upper_spread_" + dataset + ".npy", ecd_upper_spread)
+#     np.save("beta_expansion_" + dataset + ".npy", beta_expansion)
+#     np.save("m2_m1_dist_" + dataset + ".npy", m2_m1_dist)
+#     np.save("m2_radius_" + dataset + ".npy", m2_radius)
+#     np.save("m1_kink_" + dataset + ".npy", m1_kink)
+#     np.save("m1_kink_alt_" + dataset + ".npy", m1_kink_alt)
+#     np.save("nine_prime_" + dataset + ".npy", nine_prime_dist)
+#     np.save("nine_prime_pore_" + dataset + ".npy", nine_prime_pore)
+#     np.save("minus_two_prime_" + dataset + ".npy", minus_two_prime_dist)
+#     np.save("minus_two_prime_pore_" + dataset + ".npy", minus_two_prime_pore)
+#     np.save("c_loop_" + dataset + ".npy", c_loop)
+#     '''
+#     np.save("m2_spread_" + dataset + ".npy", m2_radius)
+
+    # print(list(m2_radius[1]))
+    # print(len(list(m2_radius[1])))
+
+    # plt.plot(range(0, len(list(m2_radius[1]))), list(m2_radius[1]))
+    # plt.savefig('test.png')
