59e9543531db1f7e230b6d9e98c4ac7fa3ace45c
Anton Jansen <antonjansen96@gmail.com>
2024-03-14 17:25:57 +0100

    finished
    Plot five curves per box. Every dashed curve is the mean of the running averages of the five chains within a subunit. The black curve is the mean of the four subunits, which is the mean of the running averages of the five chains...

--- analysis/revcom/protoTimeCompact.py
+++ analysis/revcom/protoTimeCompact.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python3
 
 import os
-import sys
 import numpy as np
 import MDAnalysis
 import matplotlib
 import matplotlib.pyplot as plt
-import multiprocessing as mp
 
 from science.cphmd import getLambdaFileIndices, movingDeprotonation
 from science.parsing import loadxvg, pickleDump, pickleLoad
 from science.utility import makeSuperDict
 
-matplotlib.rcParams.update({'font.size': 20})  # Set global matplotlib font size.
+matplotlib.rcParams.update({'font.size': 20})
 
-# PARAMETERS
+def getFullResName(num: int) -> str:
+    """Returns 'D13' if num=13, etc."""
+    data = ['D13', 'E14', 'E26', 'D31', 'D32', 'E35', 'D49', 'D55', 'E67', 'E69', 'E75', 'E82', 'D86', 'D88', 'D91', 'D97', 'E104', 'D115', 'D122', 'H127', 'D136', 'D145', 'E147', 'D153', 'D154', 'D161', 'E163', 'E177', 'D178', 'E181', 'D185', 'E222', 'H235', 'E243', 'E272', 'H277', 'E282']
+    for idx in range(0, len(data)):
+        if num == int(data[idx][1:]):
+            return data[idx]
 
 resids = [13, 14, 26, 31, 32, 35, 49, 55, 67, 69, 75, 82, 86, 88, 91, 97, 104, 115, 122, 127, 136, 145, 147, 153, 154, 161, 163, 177, 178, 181, 185, 222, 235, 243, 272, 277, 282]
 sims   = ['6ZGD_7', '6ZGD_4', '4HFI_7', '4HFI_4']
@@ -97,145 +100,47 @@ for resid in resids:
         ultraMean[resid][sim] = comb
         ultraSdev[resid][sim] = sdev
 
-
-resid = 13
-sim = '6ZGD_7'
-
-for rep in reps:
-    t = range(len(superMean[resid][sim][rep]))
-    x = superMean[resid][sim][rep]
-    plt.plot(t, x, label=rep)
-
-# Plot mean
-t = range(len(ultraMean[resid][sim]))
-x = ultraMean[resid][sim]
-plt.plot(t, x, label='All', color='black')
-
-# Plot sdev
-lower = [0] * len(t)
-upper = [0] * len(t)
-for idx in t:
-    lower[idx] = x[idx] - ultraSdev[resid][sim][idx]
-    upper[idx] = x[idx] + ultraSdev[resid][sim][idx]
-plt.fill_between(t, lower, upper, color='gray', alpha=0.3)
-
-plt.legend()
-plt.axis([0, 1000, -0.05, 1.05])
-plt.savefig('test.png')    
-
-sys.exit(0)
-
-
-
-
-
-
-
 blocks = [[13, 14, 31, 32, 49, 55, 67, 69], [75, 86, 88, 91, 97, 104, 115, 122], [127, 136, 145, 147, 153, 154, 161, 163], [178, 181, 185, 222, 235, 272, 277, 282]]
 
 for block in blocks:
 
-    fig, axs = plt.subplots(nresids, nsims, figsize=(17, 9), dpi=200)
+    nresids = len(block)
+    nsims = 4
 
-
-
-
-
-
-
-
-# DEFINE TASK
-def task(resid: int, dummy: any):
-
-    u = MDAnalysis.Universe("../../sims/4HFI_4/01/CA.pdb")
-
-    nresids = len(reps) + 1  # 5, add a plus one for bottom row of 'All' panels.
-    nsims = len(sims)          # 4
-
-    fig, axs = plt.subplots(nresids, nsims, figsize=(17, 9), dpi=200)
+    fig, axs = plt.subplots(nresids, nsims, figsize=(17, 18), dpi=200)
 
     for simidx in range(0, nsims):
+        
+        for residx in range(0, nresids):
 
-        # Stores the data for making the 'All' subplots.
-        allValues = makeSuperDict([list(range(0, 1100)), []])
+            subplt = axs[residx, simidx]
 
-        for repidx in range(0, nresids):
+            # Plot the four replicas
+            for rep in reps:
+                x = superMean[block[residx]][sims[simidx]][rep]
+                t = range(len(x))
+                subplt.plot(t, x, linewidth=1.2, linestyle='--')
 
-            subplt = axs[repidx, simidx]
+            # Plot the mean of the four replicas.
+            x = ultraMean[block[residx]][sims[simidx]]
+            t = range(len(x))
+            subplt.plot(t, x, linewidth=1.5, color='black')
 
-            if repidx < 4:
-
-                store = []
-                for idx in range(0, len(chains)):
-                    num = getLambdaFileIndices(universe=u, resid=resid)[idx]
-
-                    # Debug and user update.
-                    print(sims[simidx], f"{reps[repidx]:02d}", chains[idx], num)
-
-                    # Load data from associated cphmd-coord file.
-                    data = loadxvg(f"../../sims/{sims[simidx]}/{reps[repidx]:02d}/cphmd-coord-{num}.xvg", dt=1000)
-                    t = [val / 1000.0 for val in data[0]]
-
-                    # Histidine is reversed.
-                    if resid in [127, 235, 277]:
-                        x = movingDeprotonation(data[1])
-                    else:
-                        x = [1 - val for val in movingDeprotonation(data[1])]
-
-                    subplt.plot(t, x, linewidth=1.2, linestyle='--')
-                    store.append(x)
-
-                means = [0] * len(t)
-                lower = [0] * len(t)
-                upper = [0] * len(t)
-                for idx in range(len(t)):
-                    temp = [store[0][idx], store[1][idx], store[2][idx], store[3][idx], store[4][idx]]
-                    mean = np.mean(temp)
-                    sdev = np.std(temp)
-                    means[idx] = mean
-                    upper[idx] = mean + sdev
-                    lower[idx] = mean - sdev
-
-                    # Store data of the five chains for the 'All' plot.
-                    allValues[idx] += temp  
-
-                subplt.plot(t, means, color='black', linewidth=2.0)
-                subplt.fill_between(t, lower, upper, color='gray', alpha=0.3)
-
-            else:
-                means = []
-                lower = []
-                upper = []
-                finallength = 0
-
-                for idx in range(0, 1100):
-                    values = allValues[idx]
-
-                    if len(values) < 10:    # This is because not all sims ran equally long.
-                        finallength = idx   # Some stopped slightly before 1000 ns.
-                        break
-
-                    mean = np.mean(values)
-                    sdev = np.std(values)
-                    means.append(mean)
-                    lower.append(mean - sdev)
-                    upper.append(mean + sdev)
-
-                subplt.plot(range(finallength), means, color='black', linewidth=2.0)
-                subplt.fill_between(range(finallength), lower, upper, color='gray', alpha=0.3)
+            # Plot the standard deviation of the four replicas.
+            lower = [0] * len(t)
+            upper = [0] * len(t)
+            for idx in t:
+                lower[idx] = x[idx] - ultraSdev[block[residx]][sims[simidx]][idx]
+                upper[idx] = x[idx] + ultraSdev[block[residx]][sims[simidx]][idx]
+            subplt.fill_between(t, lower, upper, color='gray', alpha=0.3)
 
             # Set x-lim and y-lim.
             subplt.set_ylim([-0.05, 1.05])
             subplt.set_xlim([0, 1000])
 
-            if repidx < 4:
-                subplt.text(15, 0.85, str(repidx+1))
-            else:
-                subplt.text(15, 0.85, 'All')
-
             # If we're not in the last row, do not show the xticks.
             subplt.set_xticks([200, 400, 600, 800])
-            if repidx != nresids - 1:
+            if residx != nresids - 1:
                 subplt.set_xticklabels([])
                 subplt.xaxis.set_ticks_position('none')
             else:
@@ -247,25 +152,17 @@ def task(resid: int, dummy: any):
                 subplt.set_yticklabels([])
                 subplt.yaxis.set_ticks_position('none')
             else:
-                subplt.set_ylabel("Proto frac")
+                subplt.set_ylabel(f"{getFullResName(block[residx])}", size=24)
+                # subplt.text(-400, 0.44)
 
             # Add (horizontal) grid to all subplots.
             subplt.grid(True, linestyle='--', axis='y')
 
             # Add title to top row.
-            if repidx == 0:
+            if residx == 0:
                 subplt.set_title(fancy[simidx], size=20)
 
     fig.tight_layout(pad=0.2)
-    fig.savefig(f"proto_{resid}.png")
-    os.system(f"convert proto_{resid}.png -trim proto_{resid}.png")
+    fig.savefig(f"compact_{block[0]}.png")
+    os.system(f"convert compact_{block[0]}.png -trim compact_{block[0]}.png")
     fig.clear()
-
-# GATHER ITERABLES
-items = []
-for resid in resids:
-    items.append((resid, 1.0))
-
-# RUN MULTITHREADED
-pool = mp.Pool(processes=mp.cpu_count())
-pool.starmap(task, items, chunksize=1)
